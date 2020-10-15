745fafc3f09bc64b02886889066b3b23b62b777d
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-10-15 23:58:47 +0900

    それぞれのvpcにsubnetを作成

--- terraform/aws/vpc.tf
+++ terraform/aws/vpc.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "my-service" {
+resource "aws_vpc" "my_service" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
@@ -6,10 +6,34 @@ resource "aws_vpc" "my-service" {
   }
 }
 
-resource "aws_vpc" "external-service" {
+resource "aws_vpc" "external_service" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "external service"
+  }
+}
+
+resource "aws_subnet" "my_service_public_1a" {
+  vpc_id = aws_vpc.my_service.id
+
+  availability_zone = "ap-northeast-1a"
+
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "my_service_public_1a"
+  }
+}
+
+resource "aws_subnet" "external_service_public_1a" {
+  vpc_id = aws_vpc.external_service.id
+
+  availability_zone = "ap-northeast-1a"
+
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "external_service_public_1a"
   }
 }
