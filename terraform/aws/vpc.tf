d6cb79cacd8eefa8645cfb2c671752f59f5541f7
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-10-16 00:23:49 +0900

    route table作成

--- terraform/aws/vpc.tf
+++ terraform/aws/vpc.tf
@@ -27,6 +27,25 @@ resource "aws_internet_gateway" "my_service" {
   }
 }
 
+resource "aws_route_table" "my_service_public" {
+  vpc_id = aws_vpc.my_service.id
+
+  tags = {
+    Name = "my_service_public"
+  }
+}
+
+resource "aws_route" "my_service_public" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = aws_route_table.my_service_public.id
+  gateway_id             = aws_internet_gateway.my_service.id
+}
+
+resource "aws_route_table_association" "my_service_public_1a" {
+  subnet_id      = aws_subnet.my_service_public_1a.id
+  route_table_id = aws_route_table.my_service_public.id
+}
+
 # external_service
 resource "aws_vpc" "external_service" {
   cidr_block = "10.0.0.0/16"
@@ -54,4 +73,23 @@ resource "aws_internet_gateway" "external_service" {
   tags = {
     Name = "external_service"
   }
+}
+
+resource "aws_route_table" "external_service_public" {
+  vpc_id = aws_vpc.external_service.id
+
+  tags = {
+    Name = "external_service_public"
+  }
+}
+
+resource "aws_route" "external_service_public" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = aws_route_table.external_service_public.id
+  gateway_id             = aws_internet_gateway.external_service.id
+}
+
+resource "aws_route_table_association" "external_service_public_1a" {
+  subnet_id      = aws_subnet.external_service_public_1a.id
+  route_table_id = aws_route_table.external_service_public.id
 }
