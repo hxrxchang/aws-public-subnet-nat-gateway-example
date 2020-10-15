0de4ca2e5e682af339330c86e9e02cde54cb50b8
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-10-16 00:12:04 +0900

    igw作成

--- terraform/aws/vpc.tf
+++ terraform/aws/vpc.tf
@@ -19,6 +19,14 @@ resource "aws_subnet" "my_service_public_1a" {
   }
 }
 
+resource "aws_internet_gateway" "my_service" {
+  vpc_id = aws_vpc.my_service.id
+
+  tags = {
+    Name = "my_service"
+  }
+}
+
 # external_service
 resource "aws_vpc" "external_service" {
   cidr_block = "10.0.0.0/16"
@@ -37,5 +45,13 @@ resource "aws_subnet" "external_service_public_1a" {
 
   tags = {
     Name = "external_service_public_1a"
+  }
+}
+
+resource "aws_internet_gateway" "external_service" {
+  vpc_id = aws_vpc.external_service.id
+
+  tags = {
+    Name = "external_service"
   }
 }
