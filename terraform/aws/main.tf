05060132dca09d420c6a0e69d80c87f6390e66b5
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-10-16 10:18:59 +0900

    backendのバケットを変更

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -5,16 +5,10 @@ provider "aws" {
 terraform {
   required_version = ">= 0.13.0"
   backend "s3" {
-    bucket  = "aws-public-subnet-nat-gateway-example-tfstate"
+    bucket  = "hxrxchang-terraform-state"
     region  = "ap-northeast-1"
-    key     = "terraform.tfstate"
+    key     = "aws-public-subnet-nat-gateway-example.tfstate"
     encrypt = true
   }
 }
 
-resource "aws_s3_bucket" "tfstate" {
-  bucket = "aws-public-subnet-nat-gateway-example-tfstate"
-  versioning {
-    enabled = true
-  }
-}
