78ab2cfea1d2995ef56b55b9b1d4ea4a1c0754b4
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-10-17 00:55:44 +0900

    external_service用のsecurity groupを作成

--- terraform/aws/security_group.tf
+++ terraform/aws/security_group.tf
@@ -1,3 +1,4 @@
+# my_service
 resource "aws_security_group" "my_service_web_server_sg" {
   name        = "web_server"
   description = "Allow http and ssh"
@@ -31,16 +32,6 @@ resource "aws_security_group_rule" "my_service_sg_in_ssh" {
   security_group_id = aws_security_group.my_service_web_server_sg.id
 }
 
-resource "aws_security_group_rule" "my_service_sg_in_icmp" {
-  type        = "ingress"
-  cidr_blocks = ["0.0.0.0/0"]
-  from_port   = -1
-  to_port     = -1
-  protocol    = "icmp"
-
-  security_group_id = aws_security_group.my_service_web_server_sg.id
-}
-
 resource "aws_security_group_rule" "my_service_sg_out_all" {
   type        = "egress"
   cidr_blocks = ["0.0.0.0/0"]
@@ -49,4 +40,50 @@ resource "aws_security_group_rule" "my_service_sg_out_all" {
   protocol    = "-1"
 
   security_group_id = aws_security_group.my_service_web_server_sg.id
+}
+
+# external_service
+resource "aws_security_group" "external_service_web_server_sg" {
+  name        = "web_server"
+  description = "Allow http from my service and ssh"
+  vpc_id      = aws_vpc.external_service.id
+  tags = {
+    Name = "external_service_web_server_sg"
+  }
+}
+
+resource "aws_security_group_rule" "external_service_sg_in_http" {
+  type      = "ingress"
+  from_port = 80
+  to_port   = 80
+  protocol  = "tcp"
+  // TODO: external_serviceのHTTP接続はmy_serviceのサブネットのNAT Gatewayからのみ許可するように変更
+  cidr_blocks = [
+    "0.0.0.0/0"
+  ]
+
+  security_group_id = aws_security_group.external_service_web_server_sg.id
+}
+
+resource "aws_security_group_rule" "external_service_sg_in_ssh" {
+  type      = "ingress"
+  from_port = 22
+  to_port   = 22
+  protocol  = "tcp"
+  # sshはローカルからもできるように許可しちゃう
+  cidr_blocks = [
+    "0.0.0.0/0"
+  ]
+
+  security_group_id = aws_security_group.external_service_web_server_sg.id
+}
+
+resource "aws_security_group_rule" "external_service_sg_out_all" {
+  type        = "egress"
+  cidr_blocks = ["0.0.0.0/0"]
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+
+  security_group_id = aws_security_group.external_service_web_server_sg.id
 }
